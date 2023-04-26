ea07b5a08c277aa4563442a3240e8c58ef3d7290
Abolade Agboola <malikagboola18@gmail.com>
2023-04-26 15:44:47 +0100

    news

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import Canvas from "./canvas"
-import Design from "./pages/Design"
-import Home from "./pages/Home"
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <main className="app transition-all ease-in"> 
-      <Home/>
+    <main className="app transition-all ease-in">
+      <Home />
       <Canvas />
-      <Design />
+      <Customizer />
     </main>
   )
 }
